fba52ad34a04b160ded1f41aeeb07c792b5d3467
Mayra Malaquias <mayra.malaquias@hotmail.com>
2023-03-07 18:34:53 -0300

    :bug:Corrigindo bug

--- script.js
+++ script.js
@@ -7,25 +7,30 @@
         const altura = document.querySelector("#altura");
         if (peso.value !=="" && altura.value !==""){
             const imc = peso.value/(altura.value* altura.value);
+            console.log(imc);
             if(imc < 18.5){
                
                 resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Abaixo do Peso <img src="imagens/baixo.jpg" alt=" " class="imagen">`;
            
-            } else if(imc  == 18.5  || imc < 24.90){
+            } else if(imc  >= 18.5  && imc < 25){
                
                 resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Peso Ideal <img src="imagens/parabens.jpg" alt=" " class="imagen">`;
-            }else if(imc  == 25.00  || imc < 29.90 ){
+            }else if(imc  >= 25 && imc < 30 ){
                
                 resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Levemente acima do peso <img src="imagens/levemente.jpg" alt=" " class="imagen">`;
-            }else if(imc  == 30.00  || imc < 34.90 ){
                
-                resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Obesidade Grau I <img src="imagens/grau1.jpg" alt=" " class="imagen">`;
-            }else if(imc  == 35.00  || imc < 39.90 ){
+            }else if(imc  >= 30  && imc < 35 ){
+               
+             resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Obesidade Grau I <img src="imagens/grau1.jpg" alt=" " class="imagen">`;
+            
+            }else if(imc  >= 35  && imc < 40 ){
                
                 resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Obesidade II Severa <img src="imagens/grau2.jpg" alt=" " class="imagen">`;
-            }else if( imc < 40.00){
+               
+            }else if( imc > 40){
                
                 resultado.innerHTML= `Resultando IMC: ${imc.toFixed(2)}  \n Obesidade III Morbida <img src="imagens/grau3.jpg" alt=" " class="imagen">`;
+               
             }
         } else{
             alert("Preencha todos os campos")
